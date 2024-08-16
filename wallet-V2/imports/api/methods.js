4037100cda347f1abc947ea437b4b991b78b9637
AlvaroOkorum <alvaro.agudelo@okorum.com>
2024-08-16 08:17:34 -0500

    wallet-meteor-proyect-V2

---
+++ wallet-V2/imports/api/methods.js
@@ -0,0 +1,9 @@
+
+// import { Meteor } from 'meteor/meteor';
+// import { ContactsCollection } from './ContactsCollection';
+
+// Meteor.methods({
+//   'contacts.insert'(name, email, imageUrl) {
+//     ContactsCollection.insertAsync({ name, email, imageUrl });
+//   }
+// });
---
+++ wallet-V2/server/main.js
@@ -0,0 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import '../imports/api/ContactsCollection';
+//import '../imports/api/methods'
+
+Meteor.startup(async () => {
+});
