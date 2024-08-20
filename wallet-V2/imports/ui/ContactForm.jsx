5ed4f1e87b7ec94723394e5d01d6222cc04b69bd
AlvaroOkorum <alvaro.agudelo@okorum.com>
2024-08-19 21:36:13 -0500

    add secondary preliminary version

--- wallet-V2/imports/ui/ContactForm.jsx
+++ wallet-V2/imports/ui/ContactForm.jsx
@@ -1,32 +1,47 @@
 import React from 'react'
-import {ContactsCollection} from '../api/ContactsCollection'
-//import { Meteor } from 'meteor/meteor' 
+import { Meteor } from 'meteor/meteor'
+import { ErrorAlert } from './components/ErroAlert'
+import { SuccessAlert } from './components/SuccessAlert';
 
 export const ContactForm = () => {
 
-    const [name, setName] = React.useState('')
-    const [email, setEmail] = React.useState('')
-    const [imageUrl, setImageUrl] = React.useState('')
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [imageUrl, setImageUrl] = React.useState('');
+    const [error, setError] = React.useState('');
+    const [success, setSuccess] = React.useState('');
+
+    const showError = ({message}) => {
+      setError(message);
+      setTimeout(() => {
+        setError('')
+      }, 3000);
+    };
+
+    const showSuccess = ({message}) => {
+      setSuccess(message);
+      setTimeout(() => {
+        setSuccess('')
+      }, 3000);
+    };
 
     const saveContact = () => {
-        //console.log({name, email, imageUrl});
-        ContactsCollection.insert({name, email, imageUrl})
-        setName('')
-        setEmail('')
-        setImageUrl('')
-        // Meteor.call('contacts.insert', name, email, imageUrl, (error) => {
-        //     if (error) {
-        //       console.error('Error inserting contact:', error);
-        //     } else {
-        //       setName('');
-        //       setEmail('');
-        //       setImageUrl('');
-        //     }
-        //   });
+        Meteor.call('contacts.insert', {name, email, imageUrl}, (errorResponse) => {
+          if(errorResponse){
+            showError({message: errorResponse.error});
+          }else{
+            setName('');
+            setEmail('');
+            setImageUrl('');
+            showSuccess({ message: 'Contact saved' });
+          }
+        });
     };
 
     return (
       <form className="mt-6">
+      {error && <ErrorAlert message={error}/>}
+      {success && <SuccessAlert message={success}/>}
       <div className="grid grid-cols-6 gap-6">
         <div className="col-span-6 sm:col-span-6 lg:col-span-2">
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">
